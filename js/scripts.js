5bd8bbaad8c00c317bd6c055646466a1dd6ce17c
Brandon Kudo, Kimberly Bordon and Kyle Ebersole <kyle@>
2017-10-02 09:55:54 -0700

    Create object, and form to store info in object.

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+//back-end
+function PetList() {
+  this.list = [];
+}
+function Pet(name, type, breed, age, bio) {
+  this.petname = name;
+  this.petType = type;
+  this.petBreed = breed;
+  this.petAge = age;
+  this.petBio = bio;
+  this.petAdopted = false;
+}
+
+
+//front-end
+$(document).ready(function(){
+  var newList = new PetList();
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    var inputName = $("#input_name").val();
+    var inputType = $("input:radio[name=input_type]:checked").val();
+    var inputBreed = $("#input_breed").val();
+    var inputAge = parseInt($("#input_age").val());
+    var inputBio = $("#input_bio").val();
+
+    var newPet = new Pet(inputName, inputType, inputBreed, inputAge, inputBio);
+    console.log(newPet);
+  });
+});
